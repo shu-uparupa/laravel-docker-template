615075c1a26d9d491c2414648cc411081847aeec
shu-uparupa <u.pa.ru.pa.shiroi@gmail.com>
2026-06-29 15:30:32 +0900

    詳細画面と遷移用のボタンを実装。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -32,4 +32,12 @@ class TodoController extends Controller
 
     }
 
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+
+        return view('todo.show', ['todo' =>$todo]);
+    }
+
 }
