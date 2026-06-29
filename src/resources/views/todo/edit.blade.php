ffa937b504477c4333eab432b974818a6f0df8fc
shu-uparupa <u.pa.ru.pa.shiroi@gmail.com>
2026-06-30 03:39:56 +0900

    追加編集画面のテキストボックスを修正

--- src/resources/views/todo/edit.blade.php
+++ src/resources/views/todo/edit.blade.php
@@ -15,7 +15,6 @@
               @if($errors->has('content'))
                 <span class="text-danger">{{ $errors->first('content') }}</span>
               @endif
-              <input type="text" class="form-control" name="content" value="{{ $todo->content }}">
             </div>
           </div>
           <div class="form-group row mb-0">
