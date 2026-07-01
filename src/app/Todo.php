826945781758ea8c803cc4499feae10857a57d20
shu-uparupa <u.pa.ru.pa.shiroi@gmail.com>
2026-07-01 15:06:00 +0900

    論理削除の処理を実装。

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+    use softDeletes;
+    
     protected $tables = 'todos';
 
     protected $fillable = [
         'content',
     ];
+    
 }
 
